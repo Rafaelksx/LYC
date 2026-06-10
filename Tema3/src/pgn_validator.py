1b0756d7c09cda8c31a6e2e594683db1eb953529
Juan Longart <juandavidlongart@gmail.com>
2026-06-10 14:18:41 -0400

    feat: diseño inicial del automata y esqueleto del validador PGN

---
+++ Tema3/src/pgn_validator.py
@@ -0,0 +1,7 @@
+class PGNValidator:
+    def __init__(self):
+        self.accepting_states = {4, 5}
+
+    def transition(self, state, char):
+        # TODO: Implementar matriz de transiciones del AFD
+        return 99
