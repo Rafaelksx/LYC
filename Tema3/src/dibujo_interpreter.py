c6cf8ebefd5afa6104788cc188e28c453c775e42
Fernando Centeno <fernan2005@hotmail.com>
2026-06-10 12:56:14 -0400

    feat: estructura inicial del interprete de dibujo y validacion base

---
+++ Tema3/src/dibujo_interpreter.py
@@ -0,0 +1,9 @@
+import math
+
+class DibujoInterpreter:
+    def __init__(self, step_size=50):
+        self.step_size = step_size
+
+    def validate(self, string):
+        # TODO: Implementar validación de paréntesis y alfabeto {a, c, g, t}
+        return True, "Cadena base"
